51614d6d9753b123a653f38c00401abe6b207cf0
wiis22 <wiis22@student.bth.se>
2024-12-11 14:20:48 +0100

    Added some functions to the database

--- backend/db/mongodb/src/database.js
+++ backend/db/mongodb/src/database.js
@@ -32,7 +32,7 @@ const database = {
         const client  = await mongo.connect(dsn);
         const db = await client.db();
         const col = await db.collection(colName);
-        console.log("data inne i addOneCity db:", data);
+        console.log("data i addOne:", data, ", collection name: ", colName);
         const result = await col.insertOne(data);
 
         await client.close();
@@ -84,6 +84,89 @@ const database = {
         }
     },
 
+    /**
+     * Remove one into the collection
+     *
+     * @async
+     *
+     * @param {string} colName  Name of collection.
+     * @param {string} id       Id to be Removed into Db.
+     *
+     * @throws Error when database operation fails.
+     *
+     * @return {object} Return info about the operation.
+     */
+    deleteOne: async function deleteOne(colName, id) {
+        const client  = await mongo.connect(dsn);
+        const db = await client.db();
+        const col = await db.collection(colName);
+
+        const data = {
+            _id: new ObjectId(String(id))
+        };
+        const result = await col.deleteOne(data);
+
+        await client.close();
+
+        return result;
+    },
+
+
+    /**
+     * Update's fields by collection and id from data.
+     *
+     * @async
+     *
+     * @param {string} colName Name of collection.
+     * @param {object} data    Data containing: _id in hexadecimal format and other fields to update sould be in json format.
+     *
+     * @throws Error when database operation fails.
+     *
+     * @return {Object|null} The resould in JSON format
+     */
+    updateOne: async function updateOne(colName, data) {
+        try {
+            // if (!ObjectId.isValid(String(data.id))) {
+            //     throw new Error("ID format is not valid");
+            // }
+
+            // console.log("data", data)
+            // console.log("id", data._id)
+            // ObjectId: Needs to be super sure
+            // that id is in a correct format else it wont work.
+            const objectId = new ObjectId(String(data._id)); // this should only be done if it's a string. in api/doc-add-user it's already an ObjectId as it becomes a string when its converted to json
+            // console.log("id", data._id)
+            // console.log("objectId", objectId)
+            // console.log("here 1")
+
+            const {_id, ...updateFields } = data;
+
+            if (Object.keys(updateFields).length === 0) {
+                throw new Error("No fields provided to update")
+            }
+            //console.log("updateFields", updateFields);
+
+            const client  = await mongo.connect(dsn);
+            const db = await client.db();
+            const col = await db.collection(colName);
+            // console.log("here 2")
+
+            const result = await col.updateOne(
+                { _id: objectId },
+                { $set: updateFields }
+            );
+            // console.log("here 3")
+            await client.close();
+            // console.log('result:', result);
+            // console.log("here 4")
+            return result;
+
+
+        } catch (err) {
+            console.error("Error in updateOneDoc:", err.message);
+            throw new Error("Error updating a document");
+        }
+    },
 };
 
 module.exports = database;
