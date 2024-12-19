c2f69a514cebcf9bd8ab008d23688d4d6ba99639
Vcaze <viktor.nordenberg1@gmail.com>
2024-12-19 02:58:43 +0100

    docker-compose works to start server

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -1,21 +1,24 @@
-const jwt = require("express-jwt");
+const { expressjwt: jwt } = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 
-// Validating Auth0 tokens
-const verifyJwt = jwt({
-  // Get the signing keys from Auth0
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://YOUR_AUTH0_DOMAIN/.well-known/jwks.json`,
-  }),
+// // Validating Auth0 tokens
+// const verifyJwt = jwt({
+//   // Get the signing keys from Auth0
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://YOUR_AUTH0_DOMAIN/.well-known/jwks.json`,
+//   }),
+// 
+//   audience: "YOUR_API_IDENTIFIER",
+//   issuer: `https://YOUR_AUTH0_DOMAIN/`,
+//   algorithms: ["RS256"], // Will probably use this one, have to make sure we're using this one though
+// });
 
-  audience: "YOUR_API_IDENTIFIER",
-  issuer: `https://YOUR_AUTH0_DOMAIN/`,
-  algorithms: ["RS256"], // Will probably use this one, have to make sure we're using this one though
-});
-
-// Think response is 
+// placeholder middleweare that is always bypassed
+const verifyJwt = (req, res, next) => {
+    next();
+}
 
 module.exports = verifyJwt;
