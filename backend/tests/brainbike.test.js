a6713353f6112a9f5f5436222752b2adf58a5b85
wiis22 <wiis22@student.bth.se>
2025-01-20 15:18:46 +0100

    made some changes to bikebrain and fixed test for better coverage.

--- backend/tests/brainbike.test.js
+++ backend/tests/brainbike.test.js
@@ -4,7 +4,7 @@ const bikeBrain = require('../bike/bikeBrain');
 jest.mock('socket.io-client', () => {
     return jest.fn().mockReturnValue({
         emit: jest.fn(),
-        on: jest.fn
+        on: jest.fn()
     });
 });
 
@@ -19,47 +19,76 @@ const mockBikeData = {
     batteryPercentage: 100,
     cityData: {
         chargingStations: [{ lat: 1, lon: 0}],
-        paringZones: [{ lat: 1, lon: 0}]
+        parkingZones: [{ lat: 10, lon: 0}]
     }
 };
 
 describe('bikeBrain', () => {
     let bike;
+    let intervalSpy
+    let timeOutSpy
 
     beforeEach(() => {
+        jest.useFakeTimers();
+
+        intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
+            callback();
+            return 1;
+        });
+        timeOutSpy = jest.spyOn(global, 'setTimeout').mockImplementationOnce((callback, delay) => {
+            callback();
+            return 1;
+        });;
+
         bike = new bikeBrain(mockBikeData);
     });
 
     afterEach(() => {
+        intervalSpy.mockRestore();
+        timeOutSpy.mockRestore();
         jest.restoreAllMocks();
+        bike = null;
     });
 
     it('should create a new bike instance correctly', () => {
-        //check the instance so it has the correct data.
         expect(bike.id).toBe(mockBikeData._id);
         expect(bike.city).toBe(mockBikeData.city);
         expect(bike.available).toBe(true);
         expect(bike.batteryPercentage).toBe(100);
     });
 
+    
+    it('should join room when a bike instance is created', () => {
+        expect(bike.socket.emit).toHaveBeenCalledWith('joinRoom', { roomName: mockBikeData._id });
+    });
+
+
+    it('should just return if endRide is called without a current customer', () => {
+        bike.endRide();
+
+        expect(bike.currentCustomer).toBe(null);
+        expect(bike.available).toBe(true);
+    });
+
+
     it('should start a ride and update the available status', () => {
         const customer = 'testUser1';
         bike.available = true;
-        const intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
-            callback();
-            return 1;
-        });
+        // const intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
+        //     callback();
+        //     return 1;
+        // });
 
         bike.startRide(customer); //interval på 60000
 
-        console.log("bike: ",bike);
+        // console.log("bike: ",bike);
         
 
         expect(bike.currentCustomer).toBe(customer);
         expect(bike.available).toBe(false);
         // check if the drainBattery have been called at the start of every min.
         expect(intervalSpy).toHaveBeenCalledTimes(1);
-        intervalSpy.mockRestore();
+        // intervalSpy.mockRestore();
     });
 
 
@@ -75,27 +104,39 @@ describe('bikeBrain', () => {
 
     it('should end ride and update available status', () => {
 
-        const intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
-            callback();
-            return 1;
-        });
+        // const intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
+        //     callback();
+        //     return 1;
+        // });
 
-        const timeOutSpy = jest.spyOn(global, 'setTimeout').mockImplementationOnce((callback, delay) => {
-            callback();
-            return 1;
-        });
+        // const timeOutSpy = jest.spyOn(global, 'setTimeout').mockImplementationOnce((callback, delay) => {
+        //     callback();
+        //     return 1;
+        // });
 
         bike.startRide('testUser3');
 
-        bike.endRide(); 
+        bike.endRide();
+
+        expect(intervalSpy).toHaveBeenCalledTimes(1);
 
         expect(timeOutSpy).toHaveBeenCalledTimes(2);
 
         expect(bike.currentCustomer).toBe(null);
         expect(bike.available).toBe(true);
 
-        intervalSpy.mockRestore();
-        timeOutSpy.mockRestore();
+        // intervalSpy.mockRestore();
+        // timeOutSpy.mockRestore();
+    });
+
+    it('should emit bikeEndRide if battery gets <= 10% after called', () => {
+
+        bike.batteryPercentage = 11;
+        bike.drainBattery();
+
+        expect(bike.socket.emit).toHaveBeenCalledWith("bikeEndRide", { userId: bike.currentCustomer });
+        expect(bike.operational).toBe(false);
+        expect(bike.socket.emit).toHaveBeenCalledWith("updateBike", { id: bike.id, operational: false });
     });
 
     it('should update position correctly', () => {
@@ -110,10 +151,17 @@ describe('bikeBrain', () => {
     it('should update location correctly', () => {
         const newLocation = "chargingStation";
 
-        bike.updateLocation(newLocation);
+        // const timeOutSpy = jest.spyOn(global, 'setTimeout').mockImplementationOnce((callback, delay) => {
+        //     callback();
+        //     return 1;
+        // });
+
+        bike.updateLocation(newLocation); //setTimeout på 60000
 
         expect(bike.location).toBe(newLocation);
-        expect(bike.socket.emit).toHaveBeenCalledWith("updateBike", { id: bike.id, location: newLocation })
+        expect(bike.socket.emit).toHaveBeenCalledWith("updateBike", { id: bike.id, location: newLocation });
+        expect(timeOutSpy).toHaveBeenCalledTimes(1);
+        // timeOutSpy.mockRestore();
     });
 
     it('should update available status when charging', () => {
@@ -124,16 +172,16 @@ describe('bikeBrain', () => {
     });
 
     it('should drain battery during ride', () => {
-        const intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
-            callback();
-            return 1;
-        });
+        // const intervalSpy = jest.spyOn(global, 'setInterval').mockImplementationOnce((callback, delay) => {
+        //     callback();
+        //     return 1;
+        // });
 
 
         bike.startRide('testUser4'); // interval på 60000
 
         expect(bike.batteryPercentage).toBeLessThan(100);
-        intervalSpy.mockRestore(1);
+        // intervalSpy.mockRestore(1);
     });
 
     it('should charge a drained battery to 100%', () => {
@@ -157,6 +205,20 @@ describe('bikeBrain', () => {
         const res = bike.locationInDistance(refPoint);
 
         expect(res).toBe(true);
+    });
+
+    it('should find a parkingZone if within distance', () => {
+        bike.position = { lat: 10, lon: 0}
+        bike.findLocation();
+
+        expect(bike.location).toBe("parkingZone");
+    });
+
+    it('should be in field if not within a distance of a refpoit', () => {
+        bike.position = { lat: 0, lon: 0}
+        bike.findLocation();
+
+        expect(bike.location).toBe("field");
     });
 });
 
