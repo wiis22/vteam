b08bfe32a5c594c3b068832267b759dcce74d44a
lucation <lucasdanielssonvalladares@gmail.com>
2025-01-21 17:25:53 -0500

    New toggle button to change city

--- frontend/webapp/src/admin-components/admin.js
+++ frontend/webapp/src/admin-components/admin.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 import authModel from "../models/auth"
 
 export default function Admin() {
+    const [isHidden, setIsHidden] = useState(false);
+    const [toggleButton, setToggleButton] = useState(false);
     const accessCheck = authModel.roleAccess("admin");
     document.title = 'Admin';
 
-    // checks access
-    if (accessCheck) {
-        return <div>{accessCheck}</div>;
-    }
+    useEffect(() => {
+        setIsHidden(false)
+        // checks access
+        if (accessCheck) {
+            return <div>{accessCheck}</div>;
+        }
+    }, []);
+
+    const handleClick = () => {
+        setIsHidden(!isHidden);
+        setToggleButton(!toggleButton)
+    };
 
     return (
-        <div className="chooseCity">
+        <div className="dashboard">
             <Link to="/">Till användar-sida</Link> |{" "}
             <Link to="/logout">Logga ut</Link>
             <h1>Admin sida</h1>
-                <ul>
-                    <li><Link to="/admin/goteborg">Göteborg</Link></li>
-                    <li><Link to="/admin/harnosand">Härnösand</Link></li>
-                    <li><Link to="/admin/karlskrona">Karlskrona</Link></li>
-                </ul>
+
+            <div style={{ display: isHidden ? "none" : "" }}>
+            <ul>
+                <li><Link to="/admin/goteborg" onClick={handleClick}>Göteborg</Link></li>
+                <li><Link to="/admin/harnosand" onClick={handleClick}>Härnösand</Link></li>
+                <li><Link to="/admin/karlskrona" onClick={handleClick}>Karlskrona</Link></li>
+            </ul>
+            </div>
+            {toggleButton ? (
+                <button onClick={handleClick}>
+                    Ändra stad
+                </button>
+            ): ''}
 
             <Outlet />
         </div>
