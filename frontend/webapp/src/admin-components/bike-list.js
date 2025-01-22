8238d3765f23cd6c8a41fce95b099c011418373a
lucation <lucasdanielssonvalladares@gmail.com>
2025-01-22 09:25:04 -0500

    fixed search

--- frontend/webapp/src/admin-components/bike-list.js
+++ frontend/webapp/src/admin-components/bike-list.js
@@ -108,7 +108,6 @@ export default function BikeList() {
             ))
         );
         setSearchMessage(`Du har sökt på ${searchedBike}`);
-        setSearchedBike("");
         setCurrentPage(1);
         //timer on message
         setTimeout(() => {
