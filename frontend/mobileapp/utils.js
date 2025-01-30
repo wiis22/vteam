38800f50686319859e99ac0a129930dcd26f81be
josf23 <josf23@student.bth.se>
2025-01-30 13:35:17 +0100

    toast alters size dependant on call

--- frontend/mobileapp/utils.js
+++ frontend/mobileapp/utils.js
@@ -1,17 +1,20 @@
+// declare baseURL for API requests
 const baseURL = "http://localhost:1337";
 
-
+// declare toast function to display messages to the user
 function toast(message) {
     const toast = document.getElementsByClassName("toast")[0];
 
+    toast.style.width = "";
     toast.querySelector(".toast-body").innerHTML = message;
-
     toast.classList.add("visible");
+
     setTimeout(function () {
         toast.className = toast.className.replace("visible", "");
-    },
-        3000
-    );
+    }, 3000);
+    setTimeout(function () {
+        toast.style.width = 0;
+    }, 4000);
 }
 
 export { baseURL, toast };
