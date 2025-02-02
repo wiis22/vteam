9ad688909c39937e09ec911ff3495d015edfe6f7
wiis22 <wiis22@student.bth.se>
2025-02-02 18:51:16 +0100

    Added the route to cities v2

--- frontend/mobileapp/models/cities.js
+++ frontend/mobileapp/models/cities.js
@@ -2,11 +2,7 @@ import { baseURL } from '../utils.js';
 
 const cities = {
     fetchCities: async (token) => {
-<<<<<<< HEAD
         const cityResponse = await fetch(`${baseURL}/api/v2/cities`, {
-=======
-        const cityResponse = await fetch(`${baseURL}/cities`, {
->>>>>>> 482a151e70038fcfe02521ceb7ed96c328894042
             headers: {
                 Authorization: `Bearer ${token}`,
             },
